82e193bf721db42b5a2138bc46ea8a3ed44be413
rustamkhan01 <rustamkhancode01gmail.com>
2023-02-15 22:38:23 +0530

    today solutaion

--- script.js
+++ script.js
@@ -1,114 +1,63 @@
+// 1
+// let friess = 60
+// let burgerss = 50
+// let chowminn =100
+// let manchurins = 80
+// let cokess = 50
 
-// print 1 5 15 25 125   do while for  while?
+// let friesr= Number(prompt ("How many french fries (60rs per piece) do you want to order?"));
+// let burgers= Number(prompt("How many burgers (50rs per piece) do you want to order ?"));
+// let chowmin= Number(prompt("How many plates of chowmin (100rs per plate) do you want to order?"));
+// let  manchurin = Number(prompt("How many plates of Manchurian (80rs per plate) do you want to order?"));
+// let cokes = Number( prompt("How many Cokes (50 per piece) do you want to order ?"));
 
-// for (let i = 1; i <= 100; i++){
+// console.log(60*french + 50 *burgers + 100 * chowmin + 80 *Manchurian + 50 * Cokes);
+
+//2
+// for (let i = 5; i >= 1; i--) {
+//     let a = "";
+//     for (let j = 1; j <= i; j++) {
+//       if (j % 3 === 1) {
+//         f += "$";
+//       } else {
+//         a += "#";
+//       }
+//     }
+//     console.log(a);
+// }
+
+
+
+//10
+
+// let i = "rustam"
+
+//     if(i.length<10){
 //     console.log(i)
+
+//     for (let j = 1;j<=8;j++){
+//         console.log(j)
+
+//     }
+// }
+// else{console.log("NO")
 // }
 
 
-// for (let i = 5 ; i <= 100 ;i++){
-//     if(i%5=== 0){
-//         console.log(i)
-//     }
+// 4 
+
+// let num = 100
+// switch (num%2===0){
+// case "true":
+//   console.log("odd");
+//     break;
+//     case false:
+//         console.log("even")
+//         break;
+//         case 100:
+//             console.log("100")
+//             break;
+//             default:
+//                 console.log(10000)
+//                 break;
 // }
-
-
-// for (let i = 15 ;i<= 100; i++){
-//     if (i%15===0){
-//         console.log(i)
-//     }
-// }
-
-
-// for (let i = 25;i<=500;i++){
-//     if(i%25===0){
-//         console.log(i)
-//     }
-    
-// }
-
-
-// let i = 1;
-// while(i<=100){
-//     console.log(i)
-//     i++
-// }
-
-
-
-// let i = 25
-// while( i <= 100){
-//     if( i%25===0){
-//         console.log(i)
-//     }
-// i++
-// }
-  
-
-
-// let i = 15
-// while(i<= 100){
-//     if(i%15===0){
-//         console.log(i)
-//     }
-//     i++
-// }
-
-
-// let i = 125
-// while(i<=500){
-//     if(i%125===0){
-//         console.log(i)
-//     }
-// i++
-// }
-
-
-
-// let i = 1
-// do {
-//     console.log(i)
-//     i++
-// }while(i<=20)
-
-
-
-// let i =2
-// do{
-//  if(i%2==0){ 
-//  console.log(i)
-// }  
-// i++ 
-// }while(i<=20)
-
-
-
-
-// let i = 5 
-// do {
-//     if(i%5===0){
-//         console.log(i)
-//     }
-//     i++
-// }while(i<=50)
-
-
-// let i = 25 
-// do {
-//     if(i%25===0){
-//         console.log(i)
-//     }
-//  i++
-// }while(i<=200)
-
-
-
-// let name = prompt("what is your name ");
-// let birth = Number(prompt("what is your birth date "));
-// let sisandbro = (prompt(" do you how many brodhar and sister"));
-// let hindi = Number(prompt(" enter your marks of hindi sb 10th class"));
-// let math = Number(prompt("enter your marks of math sub 10th class"));
-// let science = Number(prompt("enter your makrs of sci 10th class"));
-// console.log('my name is $ {name}  my birth date $ {birth} sister and brother $ {sisandbro} enter  hindi marks $ {hindi } enter math marks $ {math} enter sci marks ${sci}' )
-
-
