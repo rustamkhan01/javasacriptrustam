df128afb58d2c8f5445fe49da446a554f3e6028b
rustamkhan01 <rustamkhancode01gmail.com>
2023-02-15 14:58:14 +0530

    new file add

---
+++ script.js
@@ -0,0 +1,114 @@
+
+// print 1 5 15 25 125   do while for  while?
+
+// for (let i = 1; i <= 100; i++){
+//     console.log(i)
+// }
+
+
+// for (let i = 5 ; i <= 100 ;i++){
+//     if(i%5=== 0){
+//         console.log(i)
+//     }
+// }
+
+
+// for (let i = 15 ;i<= 100; i++){
+//     if (i%15===0){
+//         console.log(i)
+//     }
+// }
+
+
+// for (let i = 25;i<=500;i++){
+//     if(i%25===0){
+//         console.log(i)
+//     }
+    
+// }
+
+
+// let i = 1;
+// while(i<=100){
+//     console.log(i)
+//     i++
+// }
+
+
+
+// let i = 25
+// while( i <= 100){
+//     if( i%25===0){
+//         console.log(i)
+//     }
+// i++
+// }
+  
+
+
+// let i = 15
+// while(i<= 100){
+//     if(i%15===0){
+//         console.log(i)
+//     }
+//     i++
+// }
+
+
+// let i = 125
+// while(i<=500){
+//     if(i%125===0){
+//         console.log(i)
+//     }
+// i++
+// }
+
+
+
+// let i = 1
+// do {
+//     console.log(i)
+//     i++
+// }while(i<=20)
+
+
+
+// let i =2
+// do{
+//  if(i%2==0){ 
+//  console.log(i)
+// }  
+// i++ 
+// }while(i<=20)
+
+
+
+
+// let i = 5 
+// do {
+//     if(i%5===0){
+//         console.log(i)
+//     }
+//     i++
+// }while(i<=50)
+
+
+// let i = 25 
+// do {
+//     if(i%25===0){
+//         console.log(i)
+//     }
+//  i++
+// }while(i<=200)
+
+
+
+// let name = prompt("what is your name ");
+// let birth = Number(prompt("what is your birth date "));
+// let sisandbro = (prompt(" do you how many brodhar and sister"));
+// let hindi = Number(prompt(" enter your marks of hindi sb 10th class"));
+// let math = Number(prompt("enter your marks of math sub 10th class"));
+// let science = Number(prompt("enter your makrs of sci 10th class"));
+// console.log('my name is $ {name}  my birth date $ {birth} sister and brother $ {sisandbro} enter  hindi marks $ {hindi } enter math marks $ {math} enter sci marks ${sci}' )
+
+
